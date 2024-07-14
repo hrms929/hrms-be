df07d2fc5340f147f473342efe762240b5820355
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-07-14 12:56:05 +0530

    initial setup

--- src/shared/entities/entities.ts
+++
@@ -1,5 +0,0 @@
-import { User, } from './user.entity';
-import { UserProfile, } from './user-profile.entity';
-import { UserToken, } from './user-token.entity';
-
-export const Entities = [User, UserProfile, UserToken,];
--- src/shared/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user.entity';
-export * from './user-profile.entity';
-export * from './user-token.entity';
-
-export * from './entities';
